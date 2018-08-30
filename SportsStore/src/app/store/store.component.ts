2f5e4289e183b7af72b06b2828aba13ae8f39e46
tarasov.a <artur-tarasov@mail.ru>
2018-08-30 08:44:44 +0300

    Маршрутизация URL

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Product} from '../model/product.model';
 import {ProductRepository} from '../model/product.repository';
+import { Cart } from '../model/cart.model';
+import {Router} from '@angular/router';
 
 @Component ( {
   selector: 'app-store' ,
@@ -11,7 +13,7 @@ export class StoreComponent {
   public selectedCategory = null;       /*Выбранная катенория*/
   public productsPerPage = 4;           /*Кол-во отображанмых строк на странице*/
   public selectedPage = 1;              /*Номер выьранной страницы*/
-  constructor ( private repository: ProductRepository ) {
+  constructor ( private repository: ProductRepository, private cart: Cart, private router: Router) {
   }
 
   get products (): Product[] {
@@ -35,11 +37,10 @@ export class StoreComponent {
   }
   get pageCount(): number {
     return Math.ceil(this.repository
-      .getProducts(this.selectedCategory).length / this.productsPerPage)
+      .getProducts(this.selectedCategory).length / this.productsPerPage);
   }
-  /*get pageNumbers(): number[] {
-    return Array(Math.ceil(this.repository
-      .getProducts(this.selectedCategory).length / this.productsPerPage))
-      .fill(0).map((x, i) => i + 1);
-  }*/
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
+    this.router.navigateByUrl('/cart');
+  }
 }
